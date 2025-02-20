7e87b34350f50364f54646070ac1486603ec90e4
jseiglie <javierseiglie@gmail.com>
2025-02-20 12:37:41 +0100

    chore: added middleware to employee routes

--- routes/employees.routes.js
+++ routes/employees.routes.js
@@ -2,15 +2,35 @@ const express = require("express");
 const apiRoutes = express.Router();
 
 const employeeController = require("../controllers/employee.controller.js");
-const auth = require('../middlewares/auth.js');
+const auth = require("../middlewares/auth.js");
 const resourceCheck = require("../middlewares/resourceCheck.js");
 
-
 apiRoutes.get("test", employeeController.test);
-apiRoutes.post("/create",  employeeController.create);
-apiRoutes.get("/all",  employeeController.getAll);
-apiRoutes.get("/get/:id", resourceCheck.checkId, employeeController.getById);
-apiRoutes.put("/update",  employeeController.update);
-apiRoutes.delete("/delete",  employeeController.delete);
+apiRoutes.post(
+  "/create",
+  auth,
+  resourceCheck.exists,
+  employeeController.create
+);
+apiRoutes.get("/all", auth, employeeController.getAll);
+apiRoutes.get(
+  "/get/:id",
+  auth,
+  resourceCheck.checkId,
+  employeeController.getById
+);
+apiRoutes.put(
+  "/update",
+  auth,
+  resourceCheck.exists,
+  resourceCheck.checkId,
+  employeeController.update
+);
+apiRoutes.delete(
+  "/delete",
+  auth,
+  resourceCheck.checkId,
+  employeeController.delete
+);
 
 module.exports = apiRoutes;
